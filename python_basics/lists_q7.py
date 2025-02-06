6e41a44c8bc43d53751512b05983e6be01d7b9fe
Lionel Kanyowa <lionelkanyowa@gmail.com>
2025-02-05 20:08:21 -0500

    chore: created and modifies some fies in python_basics.

--- python_basics/lists_q7.py
+++ python_basics/lists_q7.py
@@ -7,7 +7,7 @@ list2 = [2, 6, 4]
 print(list1 == list2)
 
 # prediction: the output here will be `True`, this is because when `list1` and `list2` are 
-# evaluated using the comparison operator, the values are compared and since they are equal to 
+# evaluated using the equality operator, the values are compared and since they are equal to 
 # each other, the return value will be 'True'
 
 # When the code was ran, the output was indeed 'True'.
