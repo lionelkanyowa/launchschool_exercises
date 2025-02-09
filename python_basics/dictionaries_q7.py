e5d205c12490b758fb0f514bd316f07a633060dd
Lionel Kanyowa <lionelkanyowa@gmail.com>
2025-02-08 21:01:41 -0500

    chore: Finished dictionary exercises

---
+++ python_basics/dictionaries_q7.py
@@ -0,0 +1,24 @@
+# q.7 Create a nested dictionary that contains the following data.
+
+# Car
+# type  color   year
+# sedan blue    2003
+
+# Truck
+# type      color   year
+# pickup    red     1998
+
+vehicle = {
+    'car': {
+        'type': 'sedan',
+        'color': 'blue',
+        'year': 2003
+    },
+    'truck': {
+        'type': 'pickup',
+        'color': 'red',
+        'year': 1998,
+    },
+}
+
+print(vehicle)
---
+++ python_basics/dictionaries_q8.py
@@ -0,0 +1,14 @@
+# q.7 Rewrite `car` as a list in which each nested list contains two elements that represents the 
+# corresponding key/value pairs
+
+car = {
+    'type': 'sedan',
+    'color': 'blue',
+    'year': 2003,
+}
+
+car = [
+    ['type', 'sedan'],
+    ['color', 'blue'],
+    ['year', 2003]
+]
